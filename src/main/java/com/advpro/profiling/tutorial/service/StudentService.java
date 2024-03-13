a802e17b563e7b7e23a2fa8f85c7b4b0152fc193
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-03-13 13:39:36 +0700

    refactor: Optimize joinStudentNames method

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -40,11 +40,16 @@ public class StudentService {
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Student student : students) {
-            result += student.getName() + ", ";
+            result.append(student.getName()).append(", ");
         }
-        return result.substring(0, result.length() - 2);
+
+        if (!result.isEmpty()) {
+            result.delete(result.length() - 2, result.length());
+        }
+
+        return result.toString();
     }
 }
 
